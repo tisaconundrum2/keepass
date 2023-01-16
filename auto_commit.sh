21661abb67efe2338d6f0f16c2a64b31a2c16662
Nicholas Finch <ngf4@nau.edu>
2023-01-16 06:39:21 -0700

    Auto commit

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-# Check if the current branch has diverged
-git fetch origin
-if git rev-list HEAD..origin/master --count -gt 0; then
-    echo "Branch has diverged, not pulling"
-else
-    git pull origin master
-    git add . 
-    git commit -m "Auto commit" 
-    git push origin master
-fi
+git pull origin master
+git add . 
+git commit -m "Auto commit" 
+git push origin master
