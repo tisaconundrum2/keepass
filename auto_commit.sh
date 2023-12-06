d50142a479a14da82756b5c12ceaff0e6405422b
Nicholas Finch <ngf4@nau.edu>
2023-12-06 10:56:03 -0500

    Auto commit

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-
-while true; do
-  git pull origin master
-  git add .
-  git commit -m "Auto commit"
-  git push origin master
-  sleep 300
-done
+git pull origin master
+git add .
+git commit -m "Auto commit"
+git push origin master
