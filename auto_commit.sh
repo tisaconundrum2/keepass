d1a72b309f7358b3c7e7fa48ffda118855ad37b0
tisaconundrum2 <ngf4@nau.edu>
2026-04-17 10:18:39 -0400

    Update auto_commit.sh

--- auto_commit.sh
+++ auto_commit.sh
@@ -17,17 +17,17 @@ setup_cron() {
 perform_git_operations() {
     cd "$(dirname "$0")"
     git checkout master
-    git pull origin master
+    git pull
 
     # Check for conflicts
-    if ! git pull origin master; then
+    if ! git pull; then
         echo "Git conflict detected. No actions taken." > auto_commit.log
         exit 1
     fi
 
     git add .
     git commit -m "Auto commit"
-    git push origin master
+    git push
     echo "Auto commit done at $(date)" > auto_commit.log
 }
 
