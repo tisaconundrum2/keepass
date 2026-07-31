85ff22cf90041e52382bd1b47e79839f75ea416c
tisaconundrum2 <ngf4@nau.edu>
2026-07-31 10:05:37 -0400

    chore: trim worker branch to service code only

--- auto_commit.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-git add .
-git commit -m "Auto commit" || echo No changes to commit.
-git push --force origin master
-echo Auto commit done at %date% %time% > auto_commit.log
-exit /b 0
--- auto_commit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-git add .
-git commit -m "Auto commit" || echo "No changes to commit."
-git push --force origin master
-echo "Auto commit done at $(date)" > auto_commit.log
