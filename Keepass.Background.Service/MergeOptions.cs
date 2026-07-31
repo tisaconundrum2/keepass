ff7b81056c3af6aa65018b1192ed4e9f5d92a29f
tisaconundrum2 <ngf4@nau.edu>
2026-07-31 09:56:13 -0400

    chore: move service code to worker branch

--- Keepass.Background.Service/MergeOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace Keepass.Background.Service;
-
-public class MergeOptions
-{
-    public string BasePath { get; set; } = string.Empty;
-    public string IncomingPath { get; set; } = string.Empty;
-    public string OutputPath { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-}
